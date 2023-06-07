723a035b28023450c5f709051700c2b036c0474e
Ed Moss <ed.moss@eemengineering.com>
2023-06-07 11:42:13 +0100

    rspnding to ian's comments

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-// index.js
-
-// Import your functions
 const split_in_multiple_json_files = require('./split_in_multiple_json_files.js');
-// ...
 
-// Export your functions
 module.exports = {
   split_in_multiple_json_files,
-  // ...
 };
